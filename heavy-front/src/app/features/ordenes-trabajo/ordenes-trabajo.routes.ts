940523825e065de4bbf7ce987f9353864960cf18
Yoseth <wilfranr@gmail.com>
2026-01-24 13:33:18 -0500

    feat: crear componentes list, create y rutas de Órdenes de Trabajo
    - Componente list con tabla, filtros y acciones CRUD
    - Componente create con formulario completo
    - Rutas configuradas para list, create, edit y detail
    - Integración con servicios y NgRx

---
+++ heavy-front/src/app/features/ordenes-trabajo/ordenes-trabajo.routes.ts
@@ -0,0 +1,31 @@
+import { Routes } from '@angular/router';
+import { authGuard } from '../../core/auth/guards/auth.guard';
+
+export const ordenesTrabajoRoutes: Routes = [
+    {
+        path: '',
+        canActivate: [authGuard],
+        children: [
+            {
+                path: '',
+                loadComponent: () => import('./list/list.component').then(m => m.ListComponent),
+                title: 'Lista de Órdenes de Trabajo'
+            },
+            {
+                path: 'create',
+                loadComponent: () => import('./create/create.component').then(m => m.CreateComponent),
+                title: 'Crear Orden de Trabajo'
+            },
+            {
+                path: ':id',
+                loadComponent: () => import('./detail/detail.component').then(m => m.DetailComponent),
+                title: 'Detalle de Orden de Trabajo'
+            },
+            {
+                path: ':id/edit',
+                loadComponent: () => import('./edit/edit.component').then(m => m.EditComponent),
+                title: 'Editar Orden de Trabajo'
+            }
+        ]
+    }
+];
