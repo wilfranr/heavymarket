90cac2d94b14980de871851209743fc48b5cb94e
Yoseth <wilfranr@gmail.com>
2026-02-14 08:35:36 -0500

    feat(ui): implement custom spinner loader globally and on initial app load

---
+++ heavy-front/src/app/shared/components/loader/loader.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+    selector: 'app-loader',
+    standalone: true,
+    imports: [CommonModule],
+    template: `<div class="loader"></div>`,
+    styles: [`
+    :host {
+      display: inline-block;
+    }
+  `]
+})
+export class LoaderComponent { }
