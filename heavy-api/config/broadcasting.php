a66728a2c09e7ac11dcc0038993029f4b2b03604
Yoseth <wilfranr@gmail.com>
2026-04-20 18:03:02 -0500

    fix: configurar variables de servidor para Reverb y corregir timeouts

--- heavy-api/config/broadcasting.php
+++ heavy-api/config/broadcasting.php
@@ -36,10 +36,10 @@ return [
             'secret' => env('REVERB_APP_SECRET'),
             'app_id' => env('REVERB_APP_ID'),
             'options' => [
-                'host' => env('REVERB_HOST'),
-                'port' => env('REVERB_PORT', 443),
-                'scheme' => env('REVERB_SCHEME', 'https'),
-                'useTLS' => env('REVERB_SCHEME', 'https') === 'https',
+                'host' => env('REVERB_SERVER_HOST', env('REVERB_HOST')),
+                'port' => env('REVERB_SERVER_PORT', env('REVERB_PORT', 443)),
+                'scheme' => env('REVERB_SERVER_SCHEME', env('REVERB_SCHEME', 'https')),
+                'useTLS' => env('REVERB_SERVER_SCHEME', env('REVERB_SCHEME', 'https')) === 'https',
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
